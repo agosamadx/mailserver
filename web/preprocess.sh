9f6cd0916fa9d2493008b7ef7f1afb1a6985143b
ago <ago@autoexec.jp>
2021-04-13 10:00:41 +0900

    Set des_key

--- web/preprocess.sh
+++ web/preprocess.sh
@@ -2,10 +2,12 @@
 
 if [ -f /var/www/html/config/config.inc.php ]; then
   sed -i -e "s/MAIL_DOMAIN/${MAIL_DOMAIN}/g" /var/www/html/config/config.inc.php
+  sed -i -e "s/rcmail-\!24ByteDESkey\*Str/$(cat /dev/urandom | base64 | fold -w 24 | head -n 1)/" /var/www/html/config/config.inc.php
 fi
 if [ -f /etc/apache2/sites-available/default-ssl.conf ]; then
   sed -i -e "s/MAIL_SERVER_DOMAIN/${MAIL_SERVER_DOMAIN}/g" /etc/apache2/sites-available/default-ssl.conf
 fi
+
 
 while [ ! -f /etc/letsencrypt/live/${MAIL_SERVER_DOMAIN}/cert.pem ]; do
   sleep 1
