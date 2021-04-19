99cca789c8c9e26735736a7b747e26db121b0fbf
ago <ago@autoexec.jp>
2021-04-19 11:19:58 +0900

    Add domain

--- certbot/entrypoint.sh
+++ certbot/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -f /etc/letsencrypt/renewal/${MAIL_SERVER_DOMAIN}.conf ]; then
-  certbot --standalone --non-interactive -d ${MAIL_SERVER_DOMAIN} -m postmaster@${MAIL_DOMAIN} --agree-tos certonly
+  certbot --standalone --non-interactive -d ${MAIL_SERVER_DOMAIN} -d ${SMTP_DOMAIN} -d ${IMAP_DOMAIN} -m postmaster@${MAIL_DOMAIN} --agree-tos certonly
 fi
 
 busybox crond -l 8 -L /dev/stderr -f
