8d6eb3b19b6ca7064253cce801a14530cfb2760a
ago <ago@autoexec.jp>
2022-08-08 17:54:33 +0900

    Fix postfix startup

--- server/entrypoint-postfix.sh
+++ server/entrypoint-postfix.sh
@@ -2,7 +2,7 @@
 
 ./preprocess.sh
 
-echo ${MAIL_DOMAIN} > /etc/mailname \
- && newaliases
+echo ${MAIL_DOMAIN} > /etc/mailname && newaliases
 
+/usr/lib/postfix/configure-instance.sh
 exec /usr/sbin/postfix start-fg
