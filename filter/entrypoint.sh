71258f46e2fd5dc9b6ea45b14dc63e94646d3f03
ago <ago@autoexec.jp>
2022-08-16 11:01:59 +0900

    起動時にスパムフィルタ学習

--- filter/entrypoint.sh
+++ filter/entrypoint.sh
@@ -4,4 +4,6 @@
 
 /usr/bin/freshclam --quiet
 /usr/bin/sa-update
+/usr/bin/sa-learn --spam /home/*/Maildir/.Junk/cur
+/usr/bin/sa-learn --ham /home/*/Maildir/cur
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
