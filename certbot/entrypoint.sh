6fe06761cda3006efdbcfb016e2180eaeec5db35
ago <ago@autoexec.jp>
2022-08-16 20:35:22 +0900

    certbotの待ち受けをwebで行う

--- certbot/entrypoint.sh
+++ certbot/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -f /etc/letsencrypt/renewal/${MX_DOMAIN}.conf ]; then
-  certbot --standalone --non-interactive -d ${MX_DOMAIN} -d ${SMTP_DOMAIN} -d ${IMAP_DOMAIN} -m postmaster@${MAIL_DOMAIN} --agree-tos certonly
+  certbot certonly --webroot -w /var/www/html -d ${MX_DOMAIN} -d ${SMTP_DOMAIN} -d ${IMAP_DOMAIN} -m postmaster@${MAIL_DOMAIN} --non-interactive --agree-tos
 fi
 
 busybox crond -l 8 -L /dev/stderr -f
