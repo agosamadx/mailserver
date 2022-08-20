2e5d18c56a873e66508aae038f3760bec606fa65
ago <ago@autoexec.jp>
2022-08-20 13:58:01 +0900

    spamassassinのdbpathを修正

--- filter/entrypoint.sh
+++ filter/entrypoint.sh
@@ -4,6 +4,6 @@
 
 /usr/bin/freshclam
 /usr/bin/sa-update
-/usr/bin/sa-learn --spam /home/virtual/*/*/Maildir/.Junk/cur &
-/usr/bin/sa-learn --ham /home/virtual/*/*/Maildir/cur &
+/usr/bin/sa-learn --dbpath /var/lib/amavis/.spamassassin --spam /home/virtual/*/*/Maildir/.Junk/cur
+/usr/bin/sa-learn --dbpath /var/lib/amavis/.spamassassin --ham /home/virtual/*/*/Maildir/cur
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
