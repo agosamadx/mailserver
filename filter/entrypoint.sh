dbcfe0a8065bd77e7f3d7c555b93a8d4c8452317
ago <ago@autoexec.jp>
2022-08-22 10:36:53 +0900

    Fix sa-learn user

--- filter/entrypoint.sh
+++ filter/entrypoint.sh
@@ -6,4 +6,9 @@
 /usr/bin/sa-update
 /usr/bin/sa-learn --dbpath /var/lib/amavis/.spamassassin --spam /home/virtual/*/*/Maildir/.Junk/cur
 /usr/bin/sa-learn --dbpath /var/lib/amavis/.spamassassin --ham /home/virtual/*/*/Maildir/cur
+
+if [ -d /var/lib/amavis/.spamassassin ]; then
+  chown -R amavis:amavis /var/lib/amavis/.spamassassin
+fi
+
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
