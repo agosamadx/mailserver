6acf02e8df9689c9b3a0ec48776996418328f587
ago <ago@autoexec.jp>
2023-01-31 10:41:02 +0900

    Fix error

--- filter/entrypoint.sh
+++ filter/entrypoint.sh
@@ -2,11 +2,14 @@
 
 ./preprocess.sh
 
+if [ -d /var/lib/clamav ]; then
+  chown -R clamav:clamav /var/lib/clamav
+fi
 /usr/bin/freshclam
+
 /usr/bin/sa-update
 /usr/bin/sa-learn --dbpath /var/lib/amavis/.spamassassin --spam /home/virtual/*/*/Maildir/.Junk/cur
 /usr/bin/sa-learn --dbpath /var/lib/amavis/.spamassassin --ham /home/virtual/*/*/Maildir/cur
-
 if [ -d /var/lib/amavis/.spamassassin ]; then
   chown -R amavis:amavis /var/lib/amavis/.spamassassin
 fi
